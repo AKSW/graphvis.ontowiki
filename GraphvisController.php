c9b05e5df9f0a6e462760b038bdbd1d6e3280dc4
Tim Ermilov <yamalight@gmail.com>
2012-02-13 14:34:29 +0100

    simple tree view

---
+++ GraphvisController.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * semantic pingback controller
+ *
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_components_pingback
+ * @copyright  Copyright (c) 2011, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+require_once 'OntoWiki/Model/TitleHelper.php';
+
+class GraphvisController extends OntoWiki_Controller_Component {
+    /**
+     * receive a ping
+     */
+    public function indexAction() {
+		// disable tabs
+        OntoWiki_Navigation::disableNavigation();
+
+		// self url
+		$url = OntoWiki::getInstance()->extensionManager->getComponentUrl('graphvis');
+                                                                       
+	   	// css
+		$this->view->headLink()->appendStylesheet($url . 'resources/css/main.css');
+		// js libs
+		$this->view->headScript()->appendFile($url . 'resources/libs/d3.min.js');
+		$this->view->headScript()->appendFile($url . 'resources/libs/d3.geom.min.js');
+        $this->view->headScript()->appendFile($url . 'resources/libs/d3.layout.min.js');
+		// my js
+		$this->view->headScript()->appendFile($url . 'resources/js/main.js');
+		
+		$base = "http://aksw.org/Projects/OntoWiki";
+		$baseName = "OntoWiki";
+		$result = $this->getRelations($base, $baseName);
+		
+		$this->view->chartData = $result;
+    }
+
+	
+	public function getchildrenAction(){
+		$this->_helper->layout()->disableLayout();
+		
+		$base = $_GET['uri'];
+		$baseName = '';
+		$result = $this->getRelations($base, $baseName); 
+		
+		echo json_encode($result);
+	}
+	
+	protected function getRelations($base, $baseName){
+		$query = '
+			PREFIX aksw: <http://aksw.org/schema/> .
+			SELECT ?relation ?object 
+			WHERE {
+				<'.$base.'> ?relation ?object .
+				FILTER sameTerm(?relation, aksw:relatedProject)
+			}';
+		//echo $query; die;
+        $relationsQuery = Erfurt_Sparql_SimpleQuery :: initWithString($query);
+        $relationsResult = $this->_owApp->selectedModel->sparqlQuery($relationsQuery);
+		
+		$namesArray = array();
+		foreach($relationsResult as $res){
+			$namesArray[$res['object']] = array('name' => '', 'rel' => $res['relation']);
+		}
+		
+		// add uris to title helper
+		$titleHelper = new OntoWiki_Model_TitleHelper($this->_owApp->selectedModel);
+		$titleHelper->addResources(array_keys($namesArray));
+		
+		// get names
+		$result = array('name' => $baseName, 'children' => array());
+        foreach ($namesArray as $uri => $obj) {
+            $title = $titleHelper->getTitle($uri);
+
+            if (null !== $title) {
+                $result['children'][] = array('name' => $title, 'uri' => $uri );
+            } else {
+                $result['children'][] = array('name' => OntoWiki_Utils::compactUri($uri), 'uri' => $uri );
+            }
+        }
+
+		//echo "<pre>";
+		//print_r($result); die;
+		return $result;
+	}
+}
