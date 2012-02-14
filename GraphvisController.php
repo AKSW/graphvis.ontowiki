c5b117b74f3f0541002aae2fbf2e8ca9cdcc8365
Sebastian Tramp <mail@sebastian.tramp.name>
2012-02-14 16:38:48 +0100

    fix cs issues and doc header

--- GraphvisController.php
+++ GraphvisController.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * graph controller
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+/**
+ * graphvis controller
  *
  * @category   OntoWiki
  * @package    OntoWiki_extensions_components_graphvis
- * @copyright  Copyright (c) 2012, {@link http://aksw.org AKSW}
- * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
-
-require_once 'OntoWiki/Model/TitleHelper.php';
-
 class GraphvisController extends OntoWiki_Controller_Component
 {
     private $_relations;
--- GraphvisHelper.php
+++ GraphvisHelper.php
@@ -1,23 +1,16 @@
 <?php
-
 /**
  * This file is part of the {@link http://ontowiki.net OntoWiki} project.
  *
- * @copyright Copyright (c) 2010, {@link http://aksw.org AKSW}
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
  * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
 
 /**
- * Helper class for the Community component.
+ * graphvis helper
  *
- * - register the tab for all navigations except the instances list
- *   (this should be undone if the community tab can be created from a Query2 too)
- *
- * @category OntoWiki
- * @package Extensions
- * @subpackage Graphvis
- * @copyright Copyright (c) 2008, {@link http://aksw.org AKSW}
- * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_components_graphvis
  */
 class GraphvisHelper extends OntoWiki_Component_Helper
 {
@@ -26,23 +19,34 @@ class GraphvisHelper extends OntoWiki_Component_Helper
         // get current request info
         $request  = Zend_Controller_Front::getInstance()->getRequest();
 
-        if(($request->getControllerName() == 'resource' 
-                && $request->getActionName() == 'instances')
-          || ($request->getControllerName() == 'resource'
-                  && $request->getActionName() == 'instances'
-                  && $request->getParam('mode') == 'multi')){
-            OntoWiki_Navigation::register('graphvis', array(
-                'controller' => 'graphvis',     // history controller
-                'action'     => 'index',       // list action
-                'name'       => 'GraphVis',
-                'priority'   => 50));
+        if (
+            (
+                $request->getControllerName() == 'resource'
+                && $request->getActionName() == 'instances'
+            ) || (
+                $request->getControllerName() == 'resource'
+                && $request->getActionName() == 'instances'
+                && $request->getParam('mode') == 'multi'
+            )
+        ) {
+            OntoWiki_Navigation::register(
+                'graphvis',
+                array(
+                    'controller' => 'graphvis',
+                    'action'     => 'index',
+                    'name'       => 'GraphVis',
+                    'priority'   => 50
+                )
+            );
         } else {
-            OntoWiki_Navigation::register('graphvis', array(
-                'controller' => 'graphvis',     // history controller
-                'action'     => 'index',       // list action
-                'name'       => 'GraphVis',
-                'priority'   => 50));
+            OntoWiki_Navigation::register(
+                'graphvis', array(
+                    'controller' => 'graphvis',
+                    'action'     => 'index',
+                    'name'       => 'GraphVis',
+                    'priority'   => 50
+                )
+            );
         }
-        
     }
 }
